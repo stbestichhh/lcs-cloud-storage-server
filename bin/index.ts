ccfbf0acfa7c12b8bf1f103f3bde82e243062ba4
Kiril Yakymchuk <stbestich@gmail.com>
2024-04-03 14:18:27 +0300

    Created basic cli

--- bin/index.ts
+++ bin/index.ts
@@ -1,1 +1,20 @@
+import express from 'express';
+import { program } from 'commander';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+program.version('0.0.1-alpha', '-v, --version', 'Current version');
+program.option('-p, --port <port>', 'Tell program which port to use.').parse(process.argv);
+
+const options = program.opts();
+
+const PORT: number = options.port || process.env.PORT || 9110;
+const HOST: string = process.env.HOST || 'localhost';
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://${HOST}:${PORT}`);
+});
