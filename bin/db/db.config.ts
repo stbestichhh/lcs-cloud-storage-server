4c736188a2513a2aad4e664b6db6b872aa053b38
Kiril Yakymchuk <stbestich@gmail.com>
2024-04-03 19:57:27 +0300

    Fixed db path
    Db file had been creating in every directory, where cli called

--- bin/db/db.config.ts
+++ bin/db/db.config.ts
@@ -1,12 +1,15 @@
 import { JsonDB, Config } from 'node-json-db';
 import dotenv from 'dotenv';
+import path from 'path';
+import * as os from 'os';
 
 dotenv.config();
 
 const dbName = process.env.DB_NAME || 'lcs_db';
+const dbPath = path.join(os.homedir(), dbName);
 const saveOnPush = true;
 
-const config = new Config(dbName, saveOnPush);
+const config = new Config(dbPath, saveOnPush);
 
 export const db = new JsonDB(config);
 export const tableName = '/users';
