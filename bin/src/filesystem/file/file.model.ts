40631928f09828950e3b6223cd66f8a05cc74c4b
Kiril Yakymchuk <stbestich@gmail.com>
2024-04-07 12:33:00 +0300

    Smal refactoring and restructure

--- bin/src/filesystem/file/file.model.ts
+++
@@ -1,8 +0,0 @@
-export interface FileModel {
-  name: string;
-  content: string;
-
-  read: (path: string) => string;
-  update: (path: string) => void;
-  delete: (path: string) => void;
-}
--- bin/src/filesystem/index.ts
+++ bin/src/filesystem/index.ts
@@ -1,6 +1,5 @@
 import os from 'os';
 import path from 'path';
 
-export * from './file/file.model';
-export * from './folder/folder.model';
+export * from './models/folder.model';
 export const storageRoot = path.join(os.homedir(), '.lcs', 'storage');
