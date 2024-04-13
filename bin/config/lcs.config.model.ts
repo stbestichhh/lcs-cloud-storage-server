51b436c9fe24d64cc7690527ce5f77ed57aa4674
Kiril Yakymchuk <stbestich@gmail.com>
2024-04-13 19:33:41 +0300

    Handle error sync

--- bin/config/lcs.config.model.ts
+++ bin/config/lcs.config.model.ts
@@ -1,10 +1,15 @@
 import { ConfigType } from './lcs.config';
 import * as fs from 'fs';
 import { configPath } from './lcs.pathes';
+import { handleErrorSync } from '../src/utils';
 
 export class LcsConfig {
   static get(key: keyof ConfigType) {
-    const config = fs.readFileSync(configPath);
-    return JSON.parse(String(config))[key];
+    try {
+      const config = fs.readFileSync(configPath);
+      return JSON.parse(String(config))[key];
+    } catch (error) {
+      handleErrorSync(error, 'No config provided. Run lcs help config.');
+    }
   }
 }
--- bin/src/utils/errorHandler.ts
+++ bin/src/utils/errorHandler.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 import colors from '@colors/colors/safe';
 import { options } from '../cli';
 import * as fs from 'fs/promises';
+import * as fsSync from 'fs';
 import { logfilePath } from '../../config';
 
 export const handleServerError = async (
@@ -30,8 +31,25 @@ export const handleError = async (error: unknown, message?: string) => {
   }
 };
 
+export const handleErrorSync = (error: unknown, message?: string) => {
+  if (error && error instanceof Error) {
+    console.error(colors.red(message ?? 'Unexpected error.'));
+    console.error(error);
+  }
+
+  if (options.log) {
+    logErrorSyns(error);
+  }
+}
+
 export const logError = async (error: unknown) => {
   const date = new Date(Date.now()).toISOString();
   const logData = `[${date}] ${error}`;
   await fs.appendFile(logfilePath, logData);
 };
+
+export const logErrorSyns = (error: unknown) => {
+  const date = new Date(Date.now()).toISOString();
+  const logData = `[${date}] ${error}`;
+  fsSync.appendFileSync(logfilePath, logData);
+}
