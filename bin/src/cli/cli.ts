5298db506edc217e1c618f9a19112146e2f78d32
Kiril Yakymchuk <stbestich@gmail.com>
2024-04-20 15:52:05 +0300

    Patch 0.0.3

--- bin/src/cli/cli.ts
+++ bin/src/cli/cli.ts
@@ -4,7 +4,7 @@ import { OptionValues, program } from 'commander';
 
 program
   .name('lcs')
-  .version('0.0.2')
+  .version('0.0.3')
   .description('Local cloud storage server with authentication.')
   .allowUnknownOption();
 
