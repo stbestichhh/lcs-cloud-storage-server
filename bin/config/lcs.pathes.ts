d9f264123cb9d86eab82beb393b5442f508ad2fd
Kiril Yakymchuk <stbestich@gmail.com>
2024-04-21 12:15:15 +0300

    Updated app root dir path

--- bin/config/lcs.pathes.ts
+++ bin/config/lcs.pathes.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import os from 'os';
 
-export const storageRoot = path.join(os.homedir(), '.lcs', 'storage');
-export const configPath = path.join(os.homedir(), '.lcs', '.lcs.config');
-export const logfilePath = path.join(os.homedir(), '.lcs', '.lcs.logs');
+export const storageRoot = path.join(os.homedir(), '.lcs-cloud-storage', 'storage');
+export const configPath = path.join(os.homedir(), '.config', 'lcs-cloud-storage', 'lcs.config');
+export const logfilePath = path.join(os.homedir(), '.lcs-cloud-storage', 'lcs.logs');
