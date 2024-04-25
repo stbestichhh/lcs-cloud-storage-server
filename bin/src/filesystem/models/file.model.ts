34b26231c6e7b9a141d6cd2536ed8b94193a16b1
Kiril Yakymchuk <stbestich@gmail.com>
2024-04-25 11:25:25 +0300

    Create file in File model

--- bin/src/filesystem/models/file.model.ts
+++ bin/src/filesystem/models/file.model.ts
@@ -2,6 +2,16 @@ import * as fs from 'fs/promises';
 import { PathLike } from 'node:fs';
 
 export class File {
+  public readonly content: string;
+
+  constructor(content?: string) {
+    this.content = content || '';
+  }
+
+  async create(filePath: string) {
+    return await fs.writeFile(filePath, this.content);
+  }
+
   static async remove(dirpath: PathLike): Promise<void> {
     return await fs.rm(dirpath);
   }
