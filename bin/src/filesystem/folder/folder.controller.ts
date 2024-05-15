8d24aed1e8d1287a20563360d055fd6d5e3ea2d8
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-15 16:24:09 +0300

    Folder controller

--- bin/src/filesystem/folder/folder.controller.ts
+++ bin/src/filesystem/folder/folder.controller.ts
@@ -1,18 +1,17 @@
-import { listdir, makedir, move, removedir } from './folder.service';
-import { Request, Response } from 'express';
+import { loginValidation } from '../../middleware';
+import {
+  listdir,
+  makedir,
+  removedir,
+  move,
+} from './folder.service';
+import express from 'express';
 
-export const _listdir = async (req: Request, res: Response) => {
-  return await listdir(req, res);
-};
+export const FolderRouter = express.Router();
 
-export const _makedir = async (req: Request, res: Response) => {
-  return await makedir(req, res);
-};
-
-export const _move = async (req: Request, res: Response) => {
-  return await move(req, res);
-};
-
-export const _removedir = async (req: Request, res: Response) => {
-  return await removedir(req, res);
-};
+FolderRouter.use(loginValidation);
+FolderRouter.use('/$cmd')
+FolderRouter.get('/ls', listdir);
+FolderRouter.post('/md', makedir);
+FolderRouter.put('/mv', move);
+FolderRouter.delete('/rmrf', removedir);
