3643d5a629a328aac4814693dcacfeb2a0f583ec
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-15 13:12:09 +0300

    User entity

--- bin/db/db.config.ts
+++ bin/db/db.config.ts
@@ -1,7 +1,11 @@
 import { Sequelize } from 'sequelize';
 import { config, dbPath } from '../config';
 
-export const dbName = (config.get('dbname') || process.env.DB_NAME || 'lcs_db.sql').toString();
+export const dbName = (
+  config.get('dbname') ||
+  process.env.DB_NAME ||
+  'lcs_db.sql'
+).toString();
 
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
---
+++ bin/db/entity/user.entity.ts
@@ -0,0 +1,53 @@
+import {
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+import { sequelize } from '../db.config';
+
+export interface UserType
+  extends Model<InferCreationAttributes<UserType>, InferAttributes<UserType>> {
+  uuid: string;
+  username: string;
+  email: string;
+  password: string;
+  jti: string;
+  lastLogin: string;
+}
+
+export const UserEntity = sequelize.define<UserType>(
+  'User',
+  {
+    uuid: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    jti: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastLogin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'users',
+    timestamps: true,
+    createdAt: 'created',
+    updatedAt: 'updated',
+  },
+);
