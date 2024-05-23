2d530cce68f2934102e7fbab0fa4e6e9a0d789f9
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-23 18:57:01 +0300

    File service secondary refactoring

--- bin/src/filesystem/file/file.service.ts
+++ bin/src/filesystem/file/file.service.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import node_path from 'path';
 import * as fs from 'fs/promises';
 import { storagePath } from '../../../lib/config';
-import { handleError, isExists } from '@stlib/utils';
+import { handleError, handleErrorSync, isExists } from '@stlib/utils';
 
 export const read = async (req: Request, res: Response) => {
   try {
@@ -17,7 +17,7 @@ export const read = async (req: Request, res: Response) => {
     const filepath = node_path.join(storagePath, uuid, path);
     const fileExists = await isExists(filepath);
 
-    if (!fileExists || path === '/cat') {
+    if (!fileExists) {
       return res.status(400).json({ error: `cat: No such file or directory` });
     }
 
@@ -31,27 +31,25 @@ export const read = async (req: Request, res: Response) => {
 
     return res.status(200).json({ content });
   } catch (error) {
-    await handleError(error, () => {
-      res.status(500).json({ error: 'Internal server error '});
-    });
+    handleErrorSync(error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
 };
 
 export const create = async (req: Request, res: Response) => {
   try {
-    const { content, path } = req.body;
     const uuid = req.user.sub;
+    const { content, path } = req.body;
 
     if (!uuid) {
       return res.status(403).json({ error: 'Forbidden.' });
     }
 
     const filepath = node_path.join(storagePath, uuid, path);
-
     const cuttedFilepath = filepath.slice(0, -path.basename(filepath).length);
     const pathExists = await isExists(cuttedFilepath);
 
-    if (!pathExists || path === '/touch') {
+    if (!pathExists) {
       return res
         .status(400)
         .json({ error: `touch: No such file or directory` });
@@ -62,59 +60,61 @@ export const create = async (req: Request, res: Response) => {
 
     return res.status(201).json({ message: 'File created.' });
   } catch (error) {
-    await handleError(error, () => {
-      res.status(500).json({ error: 'Internal server error '});
-    });
+    handleErrorSync(error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
 };
 
 export const remove = async (req: Request, res: Response) => {
   try {
-    const { path } = req.body;
     const uuid = req.user.sub;
+    const { path } = req.body;
 
     if (!uuid) {
       return res.status(403).json({ error: 'Forbidden' });
     }
 
     const filePath = node_path.join(storagePath, uuid, path);
-
     const fileExists = await isExists(filePath);
 
-    if (!fileExists || req.path === '/rm') {
+    if (!fileExists) {
       return res.status(400).json({ error: 'The path does not exist.' });
     }
 
     await File.remove(filePath);
+
     return res.status(200).json({ message: 'File removed.' });
   } catch (error) {
-    await handleError(error, () => {
-      res.status(500).json({ error: 'Internal server error '});
-    });
+    handleErrorSync(error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
 };
 
 export const download = async (req: Request, res: Response) => {
-  const { path } = req.body;
-  const uuid = req.user.sub;
+  try {
+    const uuid = req.user.sub;
+    const { path } = req.body;
 
-  if (!uuid) {
-    return res.status(403).json({ error: 'Forbidden' });
-  }
+    if (!uuid) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
 
-  const filePath = path.join(storagePath, uuid, path);
+    const filePath = path.join(storagePath, uuid, path);
+    const fileExists = await isExists(filePath);
 
-  const dirExists = await isExists(filePath);
+    if (!fileExists) {
+      return res
+        .status(400)
+        .json({ error: 'download: No such file or directory' });
+    }
 
-  if (!dirExists || req.path === 'dl') {
-    return res
-      .status(400)
-      .json({ error: 'download: No such file or directory' });
-  }
-
-  return res.download(filePath, async (error) => {
-    await handleError(error, () => {
-      res.status(500).json({ error: 'Internal server error '});
+    return res.download(filePath, async (error) => {
+      return await handleError(error, () => {
+        res.status(500).json({ error: 'Internal server error '});
+      });
     });
-  });
+  } catch (error) {
+    handleErrorSync(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
