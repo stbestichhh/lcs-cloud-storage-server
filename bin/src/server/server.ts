1a75914b55b4c4e27679fbdeb1d5fe430b7bba1c
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-25 14:36:16 +0300

    Server refactoring
    Fixed: custom host not aplying

--- bin/src/server/server.ts
+++ bin/src/server/server.ts
@@ -1,35 +1,19 @@
-import { handleError, limiter } from '../utils';
-import { loginValidation } from '../middleware';
-import { AuthRouter, _getUser } from '../auth';
+import { limiter } from '../utils';
+import { AuthRouter } from '../auth';
 import { FileRouter } from '../filesystem';
-import { LcsConfig } from '../../config';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { OptionValues } from 'commander';
 import * as pem from 'pem';
 import * as https from 'https';
-
-dotenv.config();
-
-// pem.config({
-//   pathOpenSSL: certDirectory,
-// });
+import { config } from '../../lib/config';
+import { handleErrorSync } from '@stlib/utils';
 
 export const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(limiter);
 
-app.get('/', (_req, res) => {
-  res.sendStatus(200);
-});
-
-app.get('/protected', loginValidation, (_req, res) => {
-  res.sendStatus(200);
-});
-
-app.get('/me', loginValidation, _getUser);
 app.use('/auth', AuthRouter);
 app.use('/storage', FileRouter);
 
@@ -37,33 +21,34 @@ export const start = async (options: OptionValues) => {
   try {
     const PORT: number =
       options.port ||
-      Number(LcsConfig.get('dport')) ||
+      config.get('dport') ||
       process.env.PORT ||
       9110;
 
     const HOST: string =
-      options.host || LcsConfig.get('dhost') || process.env.HOST || 'localhost';
+      options.host || config.get('dhost') || process.env.HOST || 'localhost';
 
     if (options.https) {
-      const days: number = Number(options.https);
+      const days: number = options.https;
+
       return pem.createCertificate(
         { days, selfSigned: true },
         async (error, keys) => {
-          await handleError(error);
+          handleErrorSync(error, {throw: true});
 
           https
             .createServer({ key: keys.clientKey, cert: keys.certificate }, app)
-            .listen(PORT, () => {
+            .listen(PORT, HOST, () => {
               console.log(`Server listening on https://${HOST}:${PORT}`);
             });
         },
       );
     }
 
-    app.listen(PORT, () => {
+    app.listen(PORT, HOST, () => {
       console.log(`Server listening on http://${HOST}:${PORT}`);
     });
   } catch (error) {
-    await handleError(error);
+    handleErrorSync(error, {throw: true});
   }
 };
