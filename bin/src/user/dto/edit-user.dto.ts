8624b2e11fd0cde0aa13c4d7f9be35297c54e9f3
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-26 14:51:56 +0300

    user service

---
+++ bin/src/user/dto/edit-user.dto.ts
@@ -0,0 +1,4 @@
+export interface EditUserDto {
+  username?: string;
+  password?: string;
+}
--- bin/src/user/user.service.ts
+++ bin/src/user/user.service.ts
@@ -1,5 +1,57 @@
 import { Request, Response } from 'express';
+import { UserEntity } from '../../lib/db';
+import { EditUserDto } from './dto';
+import { handleErrorSync } from '@stlib/utils';
+import { hashPassword } from '../utils';
 
 export const getUser = async (req: Request, res: Response) => {
-  return res.status(200).json(req.user);
+  try {
+    const uuid = req.user.sub;
+
+    const user = await UserEntity.findOne({
+      where: {
+        uuid
+      }
+    });
+
+    if(!user) {
+      return res.status(400).json({ error: 'Forbidden.' })
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    handleErrorSync(error);
+    return res.status(500).json({ error: 'Internal server error.' });
+  }
 };
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const uuid = req.user.sub;
+    const dto: EditUserDto = req.body;
+
+    const user = await UserEntity.findOne({
+      where: {
+        uuid
+      }
+    });
+
+    if(!user) {
+      return res.status(400).json({ error: 'Forbidden.' })
+    }
+
+    const hash = dto.password ? await hashPassword(dto.password) : user.password;
+
+    user.set({
+      username: dto.username ? dto.username : user.username,
+      password: hash,
+    });
+
+    await user.save();
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    handleErrorSync(error);
+    return res.status(500).json({ error: 'Internal server error.' });
+  }
+}
