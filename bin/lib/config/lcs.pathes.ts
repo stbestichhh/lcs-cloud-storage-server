c40b6011969973254bf769cc383550f70070cd9e
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-31 19:43:35 +0300

    movbel all hardcoded names to the pathes object

--- bin/lib/config/lcs.pathes.ts
+++ bin/lib/config/lcs.pathes.ts
@@ -1,21 +1,33 @@
 import path from 'path';
 import os from 'os';
 
+export const appNames = {
+  rootdir: '.lcs-cloud-storage',
+  rootcfgdir: 'lcs-cloud-storage',
+  configdir: '.config',
+  storagedir: 'storage',
+  configfile: 'lcs.config',
+  logsfile: 'lcs.log',
+  dbname: 'lcs_db.sql'
+}
+
 export const storagePath = path.join(
   os.homedir(),
-  '.lcs-cloud-storage',
-  'storage',
+  appNames.rootdir,
+  appNames.storagedir,
 );
 
 export const configPath = path.join(
   os.homedir(),
-  '.config',
-  'lcs-cloud-storage',
-  'lcs.config',
+  appNames.configdir,
+  appNames.rootcfgdir,
+  appNames.configfile,
 );
 
 export const logfilePath = path.join(
   os.homedir(),
-  '.lcs-cloud-storage',
-  'lcs.logs',
+  appNames.rootdir,
+  appNames.logsfile,
 );
+
+export const dbPath = path.join(os.homedir(), appNames.rootdir, appNames.dbname);
